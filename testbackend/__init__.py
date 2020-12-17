5945ef86305e6ab69b11111ff8ae7cfea097b39d
Joel Christiansen <joelchristiansen@Optiver.com>
2020-12-17 16:21:08 +0100

    better, more friendly errors

--- testbackend/__init__.py
+++ testbackend/__init__.py
@@ -17,9 +17,28 @@ __all__ = ['get_requires_for_build_sdist',
 def _open_setup_script(setup_script):
     if not os.path.exists(setup_script):
         # Supply a default setup.py
+        gen_setup_cfg()
         return gen_setup()
     raise RuntimeError(
         "TestBackend is not compatible with setup.py, please migrate to setup.cfg or pyproject.toml")
+
+
+def gen_setup_cfg():
+    import configparser
+    import toml
+    config = configparser.ConfigParser()
+    if os.path.exists('setup.cfg'):
+        with open('setup.cfg') as f:
+            config.read(f)
+
+    pyproject = toml.load('pyproject.toml')
+    config['metadata'] = {k: v for k, v in pyproject['tool']['poetry'].items()
+                          if k in ('name', 'version', 'description', 'authors')}
+    with open('setup.cfg', 'w+') as f:
+        config.write(f)
+
+    with open('pyproject.toml', 'w+') as f:
+        toml.dump(pyproject, f)
 
 
 def gen_setup():
@@ -29,10 +48,13 @@ def gen_setup():
         #   b) _say_ that it is trying to generate it on stdout
         raise RuntimeError(f"No poetry.lock found in {os.getcwd()}")
     try:
+        print(os.listdir())
         out = subprocess.check_output('poetry export --without-hashes'.split(), encoding='utf-8')
     except subprocess.CalledProcessError as e:
         print(e.output)
         raise
+    if out.startswith('Warning: The lock file is not up to date with the latest changes in pyproject.toml. You may be getting outdated dependencies. Run update to update them.'):
+        raise RuntimeError('Warning: The lock file is not up to date with the latest changes in pyproject.toml. You may be getting outdated dependencies. Run poetry update to update them.')
     reqs = []
     for line in out.split('\n'):
         try:
@@ -40,10 +62,15 @@ def gen_setup():
         except pkg_resources.extern.packaging.requirements.InvalidRequirement as e:
             # Poetry export starts with this preamble:
             # --extra-index-url http://artifactory.ams.optiver.com/artifactory/api/pypi/pypi/simple
+            # or 
+            # --index-url http://artifactory.ams.optiver.com/artifactory/api/pypi/pypi/simple
             #
             # which can't be parsed. But it can be ignored!
             if e.args[0].startswith('Parse error at "\'--extra-\'":'):
                 continue
+            elif e.args[0].startswith('Parse error at "\'--index-\'":'):
+                continue
+            print(out)
             raise
     return io.StringIO(dedent(f"""\
         from setuptools import setup
@@ -56,8 +83,7 @@ def gen_setup():
 class ApplicationBuildMetaBackend(_BuildMetaBackend):
 
     def run_setup(self, setup_script='setup.py'):
-        # NOTE: This function is copy/pasted from the parent impl, the difference is that the
-        # _open_setup_script is modified above
+        # Here we are in the build directory
         __file__ = setup_script
         __name__ = '__main__'
 
@@ -66,11 +92,21 @@ class ApplicationBuildMetaBackend(_BuildMetaBackend):
 
         exec(compile(code, __file__, 'exec'), locals())
 
+    def build_sdist(self, sdist_directory, config_settings=None):
+        print("SDIST: ", sdist_directory, config_settings)
+        print("CDW: ", os.getcwd())
+        return super().build_sdist(sdist_directory, config_settings)
+
+    def build_wheel(self, wheel_directory, config_settings=None, metadata_directory=None):
+        print("WHEEL: ", wheel_directory, config_settings, metadata_directory)
+        print("CDW: ", os.getcwd())
+        return super().build_wheel(wheel_directory, config_settings, metadata_directory)
+
     def get_requires_for_build_wheel(self, config_settings=None):
-        return ['setuptools', 'wheel >= 0.25', 'poetry']
+        return ['setuptools', 'wheel >= 0.25', 'poetry', 'toml']
 
     def get_requires_for_build_sdist(self, config_settings=None):
-        return ['setuptools', 'poetry']
+        return ['setuptools', 'poetry', 'toml']
 
 
 # The primary backend
