5b84ea55ae8e61b5c5d34ba53b57b1ac73a5ce92
Joel Christiansen <joelchristiansen@Optiver.com>
2021-02-03 09:45:06 +0100

    better variable names

--- vulcan/builder.py
+++ vulcan/builder.py
@@ -24,19 +24,19 @@ def resolve_deps(install_requires: List[str], extras: Dict[str, List[str]]
     with builder as pipenv:  # type: ignore
         site_packages = next(iter(Path(str(builder._path)).glob('lib/*/site-packages')))
         pipenv.install(install_requires)
-        base_resolved = get_freeze(pipenv, site_packages)
+        base_freeze = get_freeze(pipenv, site_packages)
         if not extras_list:
             # if we have no extras, we are done here.
-            return sorted([str(req) for req in base_resolved.values()]), {}
+            return sorted([str(req) for req in base_freeze.values()]), {}
         # for the first extra, we can use the virtualenv because the extra_reqs are by definition a superset
         # of the base reqs
         extra, extra_reqs = extras_list[0]
         pipenv.install(extra_reqs)
-        resolved_extra = get_freeze(pipenv, site_packages)
-        resolved_extras = {extra: sorted([str(req) for req in resolved_extra.values()])}
+        extra_freeze = get_freeze(pipenv, site_packages)
+        resolved_extras = {extra: sorted([str(req) for req in extra_freeze.values()])}
         if len(extras_list) == 1:
             # if we have exactly 1 extra, we can get away with only using 1 venv in total
-            return sorted([str(resolved_extra[req]) for req in base_resolved.keys()]), resolved_extras
+            return sorted([str(extra_freeze[req]) for req in base_freeze.keys()]), resolved_extras
 
         # otherwise, we make one last use of this venv to get the total deps of all the extras installed at
         # the same time ( this will be used to get the actual versions of the reqs )
@@ -50,7 +50,7 @@ def resolve_deps(install_requires: List[str], extras: Dict[str, List[str]]
             # total venvs is max(1, len(extras))
             site_packages = next(iter(Path(str(builder._path)).glob('lib/*/site-packages')))
             pipenv.install(install_requires + extra_reqs)
-            freeze = get_freeze(pipenv, site_packages)
-            resolved_extras[extra] = sorted([str(all_resolved[req]) for req in freeze])
+            extra_freeze = get_freeze(pipenv, site_packages)
+            resolved_extras[extra] = sorted([str(all_resolved[req]) for req in extra_freeze])
 
-    return sorted([str(all_resolved[req]) for req in base_resolved.keys()]), resolved_extras
+    return sorted([str(all_resolved[req]) for req in base_freeze.keys()]), resolved_extras
