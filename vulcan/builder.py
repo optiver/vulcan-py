da574fb7d7b1f227bc3490bdf8758c1a45ef29fe
Joel Christiansen <joelchristiansen@Optiver.com>
2021-02-23 12:29:30 +0100

    do not use build.env package for dependency resolution tasks

--- vulcan/builder.py
+++ vulcan/builder.py
@@ -1,59 +1,45 @@
-import subprocess
+import tempfile
 from itertools import chain
-from pathlib import Path
 from typing import Dict, List, Tuple
-
-import build.env
-from pkg_resources import Requirement
-
-
-def get_freeze(env: build.env._IsolatedEnvVenvPip, site_packages: Path) -> Dict[str, Requirement]:
-    frozen = subprocess.check_output(
-        [env._pip_executable, '-m', 'pip', 'list', '--format=freeze', '--path', str(site_packages)],
-        encoding='utf-8')
-    reqs = [Requirement.parse(line) for line in frozen.split('\n') if line]
-    return {req.name: req for req in reqs}  # type: ignore
+from vulcan.isolation import create_venv
 
 
 def resolve_deps(install_requires: List[str], extras: Dict[str, List[str]]
                  ) -> Tuple[List[str], Dict[str, List[str]]]:
-    builder = build.env.IsolatedEnvBuilder()
-    pipenv: build.env._IsolatedEnvVenvPip
 
     if not install_requires and not extras:
         return [], {}
 
     extras_list = list(extras.items())
-    with builder as pipenv:  # type: ignore
-        site_packages = next(iter(Path(str(builder._path)).glob('lib/*/site-packages')))
-        pipenv.install(install_requires)
-        base_freeze = get_freeze(pipenv, site_packages)
-        if not extras_list:
-            # if we have no extras, we are done here.
-            return sorted([str(req) for req in base_freeze.values()]), {}
-        # for the first extra, we can use the virtualenv because the extra_reqs are by definition a superset
-        # of the base reqs
-        extra, extra_reqs = extras_list[0]
-        pipenv.install(extra_reqs)
-        extra_freeze = get_freeze(pipenv, site_packages)
-        resolved_extras = {extra: sorted([str(req) for req in extra_freeze.values()])}
-        if len(extras_list) == 1:
-            # if we have exactly 1 extra, we can get away with only using 1 venv in total
-            return sorted([str(extra_freeze[req]) for req in base_freeze.keys()]), resolved_extras
+    with create_venv() as pipenv:
+        with tempfile.TemporaryDirectory() as site_packages:
+            pipenv.install(site_packages, install_requires)
+            base_freeze = pipenv.freeze(site_packages)
+            if not extras_list:
+                # if we have no extras, we are done here.
+                return sorted([str(req) for req in base_freeze.values()]), {}
+            # for the first extra, we can use the virtualenv because the extra_reqs are by definition a
+            # superset of the base reqs
+            extra, extra_reqs = extras_list[0]
+            pipenv.install(site_packages, extra_reqs)
+            extra_freeze = pipenv.freeze(site_packages)
+            resolved_extras = {extra: sorted([str(req) for req in extra_freeze.values()])}
+            if len(extras_list) == 1:
+                # if we have exactly 1 extra, we can get away with only using 1 venv in total
+                return sorted([str(extra_freeze[req]) for req in base_freeze.keys()]), resolved_extras
 
-        # otherwise, we make one last use of this venv to get the total deps of all the extras installed at
-        # the same time ( this will be used to get the actual versions of the reqs )
-        pipenv.install(list(chain.from_iterable(reqs for _, reqs in extras_list[1:])))
-        all_resolved = get_freeze(pipenv, site_packages)
+            # otherwise, we make one last use of this venv to get the total deps of all the extras installed
+            # at the same time ( this will be used to get the actual versions of the reqs )
+            pipenv.install(site_packages, list(chain.from_iterable(reqs for _, reqs in extras_list[1:])))
+            all_resolved = pipenv.freeze(site_packages)
 
-    # skipping the first extra because we've already done that one.
-    for extra, extra_reqs in extras_list[1:]:
-        with builder as pipenv:  # type: ignore
-            # this is the expensive bit, because we create a new venv for each extra beyond the first, so
-            # total venvs is max(1, len(extras))
-            site_packages = next(iter(Path(str(builder._path)).glob('lib/*/site-packages')))
-            pipenv.install(install_requires + extra_reqs)
-            extra_freeze = get_freeze(pipenv, site_packages)
-            resolved_extras[extra] = sorted([str(all_resolved[req]) for req in extra_freeze])
+        # skipping the first extra because we've already done that one.
+        for extra, extra_reqs in extras_list[1:]:
+            with tempfile.TemporaryDirectory() as site_packages:
+                # this is the expensive bit, because we create a new venv for each extra beyond the first, so
+                # total venvs is max(1, len(extras))
+                pipenv.install(site_packages, install_requires + extra_reqs)
+                extra_freeze = pipenv.freeze(site_packages)
+                resolved_extras[extra] = sorted([str(all_resolved[req]) for req in extra_freeze])
 
-    return sorted([str(all_resolved[req]) for req in base_freeze.keys()]), resolved_extras
+        return sorted([str(all_resolved[req]) for req in base_freeze.keys()]), resolved_extras
---
+++ vulcan/isolation.py
@@ -0,0 +1,55 @@
+import subprocess
+import tempfile
+from contextlib import contextmanager
+from os import PathLike
+from types import SimpleNamespace
+from typing import Dict, Generator, List, Union
+from venv import EnvBuilder
+
+from pkg_resources import Requirement
+
+
+@contextmanager
+def create_venv() -> Generator['VulcanEnvBuilder', None, None]:
+    with tempfile.TemporaryDirectory(prefix='vulcan-build-') as tempdir:
+        builder = VulcanEnvBuilder(with_pip=True)
+        builder.create(tempdir)
+        yield builder
+
+
+class VulcanEnvBuilder(EnvBuilder):
+
+    def __init__(self, system_site_packages: bool = False, clear: bool = False,
+                 symlinks: bool = False, upgrade: bool = False, with_pip: bool = False, prompt: str = None):
+        self.context: SimpleNamespace
+        super().__init__(system_site_packages=system_site_packages, clear=clear, symlinks=symlinks,
+                         upgrade=upgrade, with_pip=with_pip, prompt=prompt)
+
+    def ensure_directories(self,
+                           env_dir: Union[str, bytes, 'PathLike[str]', 'PathLike[bytes]']
+                           ) -> SimpleNamespace:
+        self.context = super().ensure_directories(env_dir)
+        return self.context
+
+    def install(self, deps_dir: Union[str, bytes, 'PathLike[str]', 'PathLike[bytes]'], requirements: List[str]
+                ) -> None:
+        # install Isolated with module pip using pep517
+        if not requirements:
+            return
+        cmd = [
+            self.context.env_exe,
+            '-Im',
+            'pip',
+            'install',
+            '--use-pep517',
+            '--target',
+            str(deps_dir)] + requirements
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+
+    def freeze(self, deps_dir: Union[str, bytes, 'PathLike[str]', 'PathLike[bytes]']
+               ) -> Dict[str, Requirement]:
+        # list with the requirements.txt format only libraries installed in specifically this venv
+        cmd = [self.context.env_exe, '-Im', 'pip', 'list', '--format=freeze', '--path', str(deps_dir)]
+        frozen = subprocess.check_output(cmd, encoding='utf-8')
+        reqs = [Requirement.parse(line) for line in frozen.split('\n') if line]
+        return {req.name: req for req in reqs}  # type: ignore
