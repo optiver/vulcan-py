61db3c5bcc594306b519ddc803d62ae164656300
Joel Christiansen <joelchristiansen@Optiver.com>
2021-03-31 11:21:49 +0200

    fixing bug where conflicting reqs can be installed if the extras are specified in a specific order

--- vulcan/builder.py
+++ vulcan/builder.py
@@ -20,26 +20,16 @@ def resolve_deps(install_requires: List[str], extras: Dict[str, List[str]]
             if not extras_list:
                 # if we have no extras, we are done here.
                 return sorted([str(req) for req in base_freeze.values()]), {}
-            # for the first extra, we can use the virtualenv because the extra_reqs are by definition a
-            # superset of the base reqs
-            extra, extra_reqs = extras_list[0]
-            print(f"Building requirements for extra '{extra}'")
-            pipenv.install(site_packages, install_requires + extra_reqs)
-            extra_freeze = pipenv.freeze(site_packages)
-            resolved_extras = {extra: sorted([str(req) for req in extra_freeze.values()])}
-            if len(extras_list) == 1:
-                # if we have exactly 1 extra, we can get away with only using 1 venv in total
-                return sorted([str(extra_freeze[req]) for req in base_freeze.keys()]), resolved_extras
 
-            # otherwise, we make one last use of this venv to get the total deps of all the extras installed
-            # at the same time ( this will be used to get the actual versions of the reqs )
+        with tempfile.TemporaryDirectory() as site_packages:
             print("Building final resolved packages")
             pipenv.install(site_packages,
                            install_requires + list(chain.from_iterable(reqs for _, reqs in extras_list)))
             all_resolved = pipenv.freeze(site_packages)
 
+        resolved_extras = {}
         # skipping the first extra because we've already done that one.
-        for extra, extra_reqs in extras_list[1:]:
+        for extra, extra_reqs in extras_list:
             with tempfile.TemporaryDirectory() as site_packages:
                 # this is the expensive bit, because we create a new venv for each extra beyond the first, so
                 # total venvs is max(1, len(extras))
