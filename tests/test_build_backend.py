7a670f45848df18a2461429ffb5cfb117bf66ede
Joel Christiansen <joelchristiansen@Optiver.com>
2021-04-12 10:21:19 +0200

    better type and error checking around the to_pep508 function

--- tests/test_build_backend.py
+++ tests/test_build_backend.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from typing import Generator
 
 from pkg_resources import Requirement
+from vulcan import VulcanConfigError, to_pep508
 
+import pytest
 # it is NOT expected for these to fall out of date, unless you explicitly regenerate the test lockfile
 # in tests/data
 EXPECTED_REQS = {
@@ -33,3 +35,29 @@ class TestFixturesOutput:
 
     def test_built_wheel_final_output_format(self, built_wheel: Path) -> None:
         assert built_wheel.suffix == '.whl'
+
+
+class TestConfig:
+
+    def test_pep508_simple(self) -> None:
+        assert to_pep508('somelib', '~=1.2.3') == 'somelib~=1.2.3'
+
+    def test_pep508_dict_no_extras(self) -> None:
+        assert to_pep508('somelib', {'version': '~=1.2.3'}) == 'somelib~=1.2.3'
+
+    def test_pep508_dict_extras(self) -> None:
+        assert to_pep508('somelib', {'version': '~=1.2.3', 'extras': ['extra1', 'extra2']}
+                         ) == 'somelib[extra1,extra2]~=1.2.3'
+
+    def test_pep508_errors(self) -> None:
+        with pytest.raises(VulcanConfigError, match="version"):
+            # missing extras (optional) and version (mandatory)
+            to_pep508('somelib', {})
+
+        with pytest.raises(VulcanConfigError, match="version"):
+            # missing version
+            to_pep508('somelib', {'extras': ['someextra', 'anotherextra']})
+
+        with pytest.raises(VulcanConfigError, match="must be a dict or a string"):
+            # totally wrong type
+            to_pep508('somelib', ['someextra', 'someextra2'])  # type: ignore
--- vulcan/__init__.py
+++ vulcan/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import toml
+from typing_extensions import TypedDict
 
 
 def find_version_file(source_dir: Path) -> Optional[Path]:
@@ -11,6 +12,15 @@ def find_version_file(source_dir: Path) -> Optional[Path]:
         return next(source_dir.rglob('VERSION'))
     except StopIteration:
         return None
+
+
+class VulcanConfigError(Exception):
+    pass
+
+
+class VersionDict(TypedDict, total=False):
+    version: str
+    extras: List[str]
 
 
 @dataclass
@@ -138,10 +148,17 @@ def get_requires(lockfile: Path) -> Tuple[List[str], Dict[str, List[str]]]:
     return content['install_requires'], content['extras_require']
 
 
-def to_pep508(lib: str, req: Union[str, Dict[str, str]]) -> str:
+def to_pep508(lib: str, req: Union[str, VersionDict]) -> str:
+    if not isinstance(req, (str, dict)):
+        raise VulcanConfigError(f"Invalid requirement {req} -- must be a dict or a string")
     if isinstance(req, str):
         # e.g. "options_sdk", "~=1.2.3" -> "options_sdk~=1.2.3"
         return f'{lib}{req}'
-    extras = f'[{",".join(req["extras"])}]' if 'extras' in req else ''
-    # "options_sdk", {"version": "~=1.2.3", "extras="networkx,git"} -> "options_sdk[networkx,git]~=1.2.3"
-    return f'{lib}{extras}{req["version"]}'
+    try:
+        extras = f'[{",".join(req["extras"])}]' if 'extras' in req else ''
+        # "options_sdk", {"version": "~=1.2.3", "extras"=["networkx","git"]}
+        #                           -> "options_sdk[networkx,git]~=1.2.3"
+        # "options_sdk", {"version": "~=1.2.3"} -> "options_sdk~=1.2.3"
+        return f'{lib}{extras}{req["version"]}'
+    except KeyError as e:
+        raise VulcanConfigError(f'invalid requirement {lib} ({req}) -- {e}') from e
