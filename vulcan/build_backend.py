1297df6f558023b0530468c1a6bdb884b26fb4a7
Joel Christiansen <joelchristiansen@Optiver.com>
2021-05-25 13:21:03 +0200

    not guarenteed to be any kind of entry point whatsoever

--- vulcan/build_backend.py
+++ vulcan/build_backend.py
@@ -50,7 +50,7 @@ try:
             if 'gui-scripts' in parsed_kwargs:
                 parsed_kwargs['entry_points']['gui_scripts'] = parsed_kwargs['gui-scripts']
                 del parsed_kwargs['gui-scripts']
-            for ep_group in list(parsed_kwargs['entry_points']):
+            for ep_group in list(parsed_kwargs.get('entry_points', {})):
                 parsed_kwargs['entry_points'][ep_group] = [
                     f'{k}={v}' for k, v in parsed_kwargs['entry_points'][ep_group].items()]
             return setuptools.setup(**parsed_kwargs)
