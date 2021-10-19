2e6aee566f60cd0a32dee93122203df8107cc890
Joel Christiansen <joelchristiansen@Optiver.com>
2021-10-19 08:56:49 +0200

    fix tests

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -61,14 +61,13 @@ class TestCli:
     def test_lock_without_lockfile_succeeds(self, runner: CliRunner, test_application: Path) -> None:
         with cd(test_application):
             (test_application / 'vulcan.lock').unlink()
+            successful(runner.invoke(cli.main, ['lock']))
 
     def test_build_without_lockfile_fails(self, runner: CliRunner, test_application: Path) -> None:
         with cd(test_application):
             (test_application / 'vulcan.lock').unlink()
-            res = runner.invoke(cli.main, ['build', '--shiv', '-o', 'dist'])
+            res = runner.invoke(cli.main, ['build', '--wheel'])
         assert res.exit_code != 0
-        assert 'Exected lockfile /' in res.stdout
-        assert 'vulcan.lock ,does not exist' in res.stdout
 
 
 @contextmanager
